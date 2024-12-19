3041754cbbf6b0148f4059e9084043d4990283ec
Miguel-MultiSafepay <104830395+Miguel-MultiSafepay@users.noreply.github.com>
2024-12-19 13:43:58 +0100

    PLGDC8-82: Remove iDEAL issuers (#63)

--- src/Plugin/Commerce/PaymentGateway/Ideal.php
+++ src/Plugin/Commerce/PaymentGateway/Ideal.php
@@ -3,7 +3,6 @@ namespace Drupal\commerce_multisafepay_payments\Plugin\Commerce\PaymentGateway;
 
 use Drupal\commerce_multisafepay_payments\Helpers\GatewayStandardMethodsHelper;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsRefundsInterface;
-use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides the Off-Site payment gateway.
@@ -17,7 +16,7 @@ use Drupal\Core\Form\FormStateInterface;
  *   },
  *   forms = {
  *     "offsite-payment" =
- *     "Drupal\commerce_multisafepay_payments\PluginForm\Ideal\IdealForm",
+ *     "Drupal\commerce_multisafepay_payments\PluginForm\StandardPayment\StandardPaymentForm",
  *   },
  * )
  */
@@ -25,30 +24,4 @@ class Ideal extends GatewayStandardMethodsHelper implements
     SupportsRefundsInterface
 {
 
-  /**
-   * Build the unique iDeal configuration form.
-   *
-   * @param array $form
-   *   The form details.
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *   Form state.
-   *
-   * @return array
-   *   Added condition
-   */
-    public function buildConfigurationForm(
-        array $form,
-        FormStateInterface $form_state
-    ) {
-      // Make the Condition.
-        $this->mspConditionHelper->orderCurrencyCondition('Euro');
-
-      // Build default form.
-        $form = parent::buildConfigurationForm($form, $form_state);
-
-      // Make a message.
-        $form['details'] = $this->mspConditionHelper->orderConditionMessage();
-
-        return $form;
-    }
 }
